68b8dde612eaf15700f7f490445b2c86b5dd0b67
Adam-Shawcross <Adam.Shawcross@academytrainee.com>
2019-09-24 15:52:23 +0100

    corrected the status code for alert messages

--- WebDev/TablePage/tablescript.js
+++ WebDev/TablePage/tablescript.js
@@ -40,7 +40,7 @@ function updatePlayer(formDataObj) {
         if (xhr.readyState === 4 && xhr.status === 200) {
             location.href = "../TablePage.html";
             getAllPlayers();
-        } else if (xhr.status === 4 && xhr.status === 500) {
+        } else if (xhr.status === 500) {
             alert("Please enter a valid ID")
         } else {
             alert("Please enter the ID of which you wish to update");
